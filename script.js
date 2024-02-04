502085612cd36f23df211dafdaf0c67338b8c9f6
taynadad25 <taynarad25@gmail.com>
2024-02-04 11:39:35 -0300

    random btn não

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function Beijos() {
-    console.log("Sim")
+    console.log("Sim");
     var img = document.createElement("IMG");
     img.src = "05b4369c0d8d79bca1452a74fb6a5cbe.gif";
-    document.getElementById('sim').appendChild(img);
+    setTimeout(() => {
+        document.getElementById('novaDiv').appendChild(img);
+    }, 500);
 }
 
-function Reset() {
-    console.log("Não")
-    alert("Você não pode clicar nesse botão! 😭😭")
-    location.reload()
+function Limpar() {
+    var div = document.getElementById('novaDiv');
+    for (child of div.children){
+        child.remove();
+    }
+}
+
+function Reset(btn) {
+    console.log("Não");
+    if(Math.random() > 0.5){
+        var width = 50;
+        var height = 50;
+    }else{
+        var width = 100;
+        var height = 100; 
+    }
+  
+    btn.style.position = "absolute";
+    btn.style.top = Math.random() * width + "vh";
+    btn.style.left = Math.random() * height + "vh";
 }
